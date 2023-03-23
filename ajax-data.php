63dbb73f7b202de8b750988df7d9d5de05e54a65
Developer Hasan <67751842+DeveloperHasanBD@users.noreply.github.com>
2023-03-23 12:08:56 +0600

    Update ajax-data.php

--- ajax-data.php
+++ ajax-data.php
@@ -1,3 +1,10 @@
+<!-- Enqueue  -->
+wp_enqueue_script( 'enq-ajax-data', get_template_directory_uri() . '/assets/js/ajax-data.js', array(), $js_version, true );
+
+wp_localize_script('enq-ajax-data', 'action_url_ajax', [
+    'ajax_url' => admin_url('admin-ajax.php'),
+]);
+<!-- end enqueue  -->
 
 function card_items_search()
 {
